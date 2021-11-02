4c409e431950b8ba0e545e9e3cca78fd62c7d5bb
MateoAgudGarcia <mateo.agudelo80@eia.edu.co>
2021-11-02 12:26:31 -0500

    refactor: define variables as private to clean code
    Divide different scopes in order to apply clean code

--- app/src/main/java/com/google/search/BrowserInitializer.java
+++ app/src/main/java/com/google/search/BrowserInitializer.java
@@ -12,20 +12,36 @@ import java.util.List;
 
 public class BrowserInitializer {
 
+
+    private static final String MICROSOFT_EDGE = "Microsoft Edge";
+    private static final String GOOGLE_CHROME = "Google Chrome";
     private static final List<String> OPTIONS_PARAMETERS
             = Arrays.asList("--headless", "--disable-gpu", "--lang=es-co");
 
-    public static WebDriver ChromeWebDriver() {
+    private static WebDriver chromeWebDriver() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments(OPTIONS_PARAMETERS);
         return new ChromeDriver(options);
     }
 
-    public static WebDriver EdgeWebDriver() {
+    private static WebDriver edgeWebDriver() {
         WebDriverManager.edgedriver().setup();
         EdgeOptions options = new EdgeOptions();
         options.addArguments(OPTIONS_PARAMETERS);
         return new EdgeDriver(options);
+    }
+
+    public static WebDriver selectWebDriver(String browser){
+        switch (browser) {
+            case MICROSOFT_EDGE:
+                return edgeWebDriver();
+            case GOOGLE_CHROME:
+                return chromeWebDriver();
+            default:
+                System.err.println("Browser has not been selected");
+                break;
+        }
+        return null;
     }
 }
--- app/src/test/java/com/google/search/SearchSteps.java
+++ app/src/test/java/com/google/search/SearchSteps.java
@@ -11,39 +11,18 @@ import org.openqa.selenium.WebDriver;
 
 import java.util.List;
 
+import static com.google.search.BrowserInitializer.selectWebDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchSteps {
 
-    public static final String MICROSOFT_EDGE = "Microsoft Edge";
-    public static final String GOOGLE_CHROME = "Google Chrome";
     WebDriver webDriver;
     MainPage mainPage;
     ResultPage resultPage;
 
     @Given("a driver for {string} is configured")
     public void aDriverForIsConfigured(String browser) {
-        switch (browser) {
-            case MICROSOFT_EDGE:
-                webDriver = BrowserInitializer.EdgeWebDriver();
-                break;
-            case GOOGLE_CHROME:
-                webDriver = BrowserInitializer.ChromeWebDriver();
-                break;
-            default:
-                System.err.println("Browser has not been selected");
-                break;
-        }
-    }
-
-    @Given("a driver for Google Chrome is configured")
-    public void aDriverForGoogleChromeIsConfigured() {
-
-    }
-
-    @Given("a driver for Microsoft Edge is configured")
-    public void aDriverForMicrosoftEdgeIsConfigured() {
-
+        webDriver = selectWebDriver(browser);
     }
 
     @When("the Google website is opened")
